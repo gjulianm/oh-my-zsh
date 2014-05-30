0abc63ff4b9e41639acf8c23924a8c1af1793c40
Guillermo Julián <gjulian93@gmail.com>
2014-05-30 09:59:09 +0200

    Changed upgrade script to use custom repo

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,7 +1,8 @@
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd "$ZSH"
-if git pull --rebase --stat origin master
+if git pull upstream master
 then
+  git push origin master
   printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
   printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
   printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
